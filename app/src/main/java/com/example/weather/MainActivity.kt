03c3e2383c9202f4d3479d743dbd4ddf9d4d4308
clickonuser <clickonuser@gmail.com>
2024-05-02 15:49:22 +0300

    Implemented retrofit and provided Internet permission.

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.weather.network.RetrofitHelper
+import com.example.weather.network.WeatherApi
 
 class MainActivity : AppCompatActivity() {
+
+    // For API requests
+    private val retrofitClient = RetrofitHelper.getInstance().create(WeatherApi::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
