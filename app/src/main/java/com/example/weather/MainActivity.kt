fa75bd21b25ea13962750b1873ed0b00dece765c
clickonuser <clickonuser@gmail.com>
2024-05-03 16:26:59 +0300

    Created fragments ViewPager2 and TabLayout. Combined them into a common work.

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -2,16 +2,50 @@ package com.example.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.example.weather.databinding.ActivityMainBinding
 import com.example.weather.network.RetrofitHelper
 import com.example.weather.network.WeatherApi
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
     // For API requests
     private val retrofitClient = RetrofitHelper.getInstance().create(WeatherApi::class.java)
 
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+        prepareViewPager()
     }
+
+    private fun initViews() {
+        viewPager = binding.viewPager
+        tabLayout = binding.tabLayout
+    }
+
+    private fun prepareViewPager() {
+        val fragmentList = arrayListOf(
+            WeatherFragment.newInstance(),
+            ForecastFragment.newInstance(),
+        )
+        val tabTitlesArray = arrayOf("Weather", "Forecast")
+
+        viewPager.adapter = ViewPagerAdapter(this, fragmentList)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabTitlesArray[position]
+        }.attach()
+    }
+
 }
